fc8cc0e1e22ac9f4828ed8cbe44cd28bc73a3121
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-12-03 15:58:36 +0530

    Service Interface created

---
+++ GL.ProjectManagement/GL.ProjectManagement.API/Interfaces/ITaskService.cs
@@ -0,0 +1,18 @@
+﻿using GL.ProjectManagement.API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GL.ProjectManagement.API.Interfaces
+{
+    interface ITaskService
+    {
+        public Task<List<TaskInfo>> GetAllTasks();
+        public Task<TaskInfo> GetTask(string id);
+        public Task<string> CreateTask(TaskCreation newtask);
+        public Task<string> UpdateTask(TaskUpdate UpdatedTask);
+        public Task<bool> DeleteTask(string id);
+        
+    }
+}
---
+++ GL.ProjectManagement/GL.ProjectManagement.API/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using GL.ProjectManagement.API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GL.ProjectManagement.API.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<List<UserInfo>> GetAllUsers();
+        public Task<UserInfo> GetUser(string id);
+        public Task<string> CreateUser(UserCreation user);
+        public Task<string> UpdateUser(UserUpdate updatedUser);
+        public Task<bool> DeleteUser(string id);
+    }
+}
