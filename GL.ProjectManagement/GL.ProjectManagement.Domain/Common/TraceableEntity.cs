94b5f2fbe37bdd4c3d52e892e4c4fce2a3d4b802
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-12-03 13:39:54 +0530

    Extracted Traceability into abstract class

---
+++ GL.ProjectManagement/GL.ProjectManagement.Domain/Common/TraceableEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GL.ProjectManagement.Domain.Common
+{
+    public interface ITraceableEntity
+    {
+        public DateTime CreatedOn { get; set; }
+    }
+}
--- GL.ProjectManagement/GL.ProjectManagement.Domain/Entities/Project.cs
+++ GL.ProjectManagement/GL.ProjectManagement.Domain/Entities/Project.cs
@@ -1,15 +1,14 @@
-﻿using System;
+﻿using GL.ProjectManagement.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GL.ProjectManagement.Domain.Entities
 {
-    public class Project
+    public class Project : TraceableEntity
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string Detail { get; set; }
-
-        public DateTime CreatedOn { get; set; }
     }
 }
